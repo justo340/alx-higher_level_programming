08e008a96174ef832a05774944bd4f32628fe231
njogu <justusnjogu6@gmail.com>
2022-12-07 12:35:01 -0800

    fix betty issues

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
     """
     if roman_string is None or type(roman_string) is not str:
         return (0)
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_list = list(roman_string.upper())
     result = 0
     prev = 0
