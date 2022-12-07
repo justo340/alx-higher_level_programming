5943df8e26f5b5651927fb07925ed14ff676753c
njogu <justusnjogu6@gmail.com>
2022-12-07 12:15:58 -0800

    fix betty issues

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,4 +6,4 @@ def update_dictionary(a_dictionary, key, value):
     updates or add key/value into a dictionary and returns a new copy
     """
     a_dictionary[key] = value
-        return (a_dictionary)
+    return (a_dictionary)
