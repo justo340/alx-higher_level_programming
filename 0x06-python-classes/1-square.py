c36a3c61b981bda3c127c0edc83e827de0fc031b
njogu <justusnjogu6@gmail.com>
2022-12-20 05:24:06 -0800

    starting out with classes in python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
+"""Define a class Square."""
 
 class Square:
     """
     class Square with private instance attribute size
     """
     def __init__(self, size):
+         """Initialize a new Square.
+         Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
