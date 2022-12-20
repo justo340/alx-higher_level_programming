c83c9bc9c5cd0e0e6fe841d0e42061564f53bb70
njogu <justusnjogu6@gmail.com>
2022-12-20 03:42:55 -0800

    getting started with python exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
     """
     divides two integers and prints the result
-    catches divide by zero exception
-    """
+    catches divide by zero exception """
     try:
         res = a / b
     except:
