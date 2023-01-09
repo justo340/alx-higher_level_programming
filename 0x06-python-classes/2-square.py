cf07b979229ac752eca8c735bd6aed98f903f6f7
justo340 <justusnjogu16@gmail.com>
2023-01-09 03:36:24 -0800

    add comment

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """Define a class Square."""
 
 
@@ -12,3 +13,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """return the area of the square"""
+        return (self.___size * self.__size)
