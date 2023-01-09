f63d686b6bb83bface40ec110a9eefb6252b42ba
justo340 <justusnjogu16@gmail.com>
2023-01-09 03:21:38 -0800

    add comment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,10 @@
 """Define a class Square."""
 
 class Square:
-    """
-    class Square with private instance attribute size
-    """
+    """ class Square with private instance attribute size"""
     def __init__(self, size):
-         """Initialize a new Square.
-         Args:
+        """Initialize a new Square.
+        Args:
             size (int): The size of the new square.
         """
         self.__size = size
