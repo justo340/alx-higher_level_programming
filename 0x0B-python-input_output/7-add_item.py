6691d21d10f0dc3f49c9d182b28e7d9930aa63e5
justo340 <justusnjogu16@gmail.com>
2023-01-10 10:49:08 -0800

    working with python input and output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-''' function that writes an Object to a text file, using a JSON representation
-'''
-import json
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    ''' module save_to_json_file
-    accepts Python object and sends JSON representation to file
-    '''
-    with open(filename, 'w') as f:
-        f.write(json.dumps(my_obj))
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
