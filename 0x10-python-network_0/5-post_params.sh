19b26f97f08a4955b5bbe82a6d1b2bb83ec0729f
justo340 <justusnjogu16@gmail.com>
2023-03-30 15:34:44 -0700

    testing my ttp script writing

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script sends POST to URL
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 
