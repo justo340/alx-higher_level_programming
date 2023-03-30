351231993291215cb48fa2b1e859108f73ae33a8
justo340 <justusnjogu16@gmail.com>
2023-03-30 15:30:14 -0700

    testing my ttp script writing

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me and get the response
-curl -sX PUT -L -d "user_id=98" -H "Origin:i got you" "0.0.0.0:5000/catch_me"
+curl -sLX PUT  -d "user_id=98" -H "Origin:school" "0.0.0.0:5000/catch_me"
